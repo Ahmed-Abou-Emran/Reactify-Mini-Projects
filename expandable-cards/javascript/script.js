3192ffce8badd79b848c157ccd6cc714023829c2
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-08-10 00:25:17 +0300

    adding mouseover event and media query

--- expandable-cards/javascript/script.js
+++ expandable-cards/javascript/script.js
@@ -1,10 +1,34 @@
 const panels = document.querySelectorAll(".panel");
 
-panels.forEach((panel) => {
-  panel.addEventListener("click", (e) => {
-    panels.forEach((panel) => {
-      panel.classList.remove("active");
+// panels.forEach((panel) => {
+//   panel.addEventListener("click", (e) => {
+//     panels.forEach((panel) => {
+//       panel.classList.remove("active");
+//     });
+//     panel.classList.add("active");
+//   });
+// });
+
+// panels.forEach((panel) => {
+//   panel.addEventListener("mouseover", (e) => {
+//     panels.forEach((panel) => {
+//       panel.classList.remove("active");
+//     });
+//     panel.classList.add("active");
+//   });
+// });
+
+const handleEventListenr = (elements, event) => {
+  elements.forEach((element) => {
+    element.addEventListener(event, (e) => {
+      elements.forEach((element) => {
+        element.classList.remove("active");
+      });
+
+      element.classList.add("active");
     });
-    panel.classList.add("active");
   });
-});
+};
+
+handleEventListenr(panels, "mouseover");
+handleEventListenr(panels, "click");
