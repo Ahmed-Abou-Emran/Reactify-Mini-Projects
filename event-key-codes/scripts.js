9890babbac7d263441291ca0ba4aab53be8f33b2
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-08-29 21:56:46 +0300

    update

--- event-key-codes/scripts.js
+++ event-key-codes/scripts.js
@@ -2,11 +2,10 @@ const entryMessageEle = document.querySelector(".entry-message");
 const eventEle = document.querySelector(".event");
 const properties = document.querySelectorAll(".property-value");
 
-console.log(event);
 window.addEventListener("keydown", (event) => {
   entryMessageEle.classList.add("hidden");
   eventEle.classList.remove("hidden");
-  properties[0].innerText = `  ${event.key === " " ? "Space" : event.key}`;
-  properties[1].innerText = `${event.keyCode}`;
-  properties[2].innerText = `${event.code}`;
+  properties[0].innerText = event.key === " " ? "Space" : event.key;
+  properties[1].innerText = event.keyCode;
+  properties[2].innerText = event.code;
 });
