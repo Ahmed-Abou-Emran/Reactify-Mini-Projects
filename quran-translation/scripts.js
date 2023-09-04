05cdb09f5b4fff67a5e65ccd11a2f0858a5864de
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-09-04 13:57:41 +0300

    commenting not working API

--- quran-translation/scripts.js
+++ quran-translation/scripts.js
@@ -3,33 +3,33 @@ const ayahTranslation = document.querySelector(".ayah-translation");
 const ayahBtn = document.querySelector(".ayahBtn");
 
 // from Al-Emrann (Surah 2)
-// const getAyah = async () => {
-//   const res = await fetch(
-//     `https://quranenc.com/api/v1/translation/aya/english_saheeh/2/${getRandomAyah()}`
-//   );
-//   const data = await res.json();
-//   console.log(data);
-
-//   ayah.innerHTML = data.result.arabic_text;
-//   ayahTranslation.innerHTML = data.result.translation;
-// };
-
-// const getRandomAyah = (surahNo) => {
-//   return Math.ceil(Math.random() * 200) - 1;
-// };
 const getAyah = async () => {
   const res = await fetch(
-    `https://tafsir.app/get.php?src=saadi&s=2&a=3&ver=1`,
-    { mode: "no-cors" }
+    `https://quranenc.com/api/v1/translation/aya/english_saheeh/2/${getRandomAyah()}`
   );
   const data = await res.json();
   console.log(data);
 
-  ayah.innerHTML = data;
+  ayah.innerHTML = data.result.arabic_text;
+  ayahTranslation.innerHTML = data.result.translation;
 };
 
 const getRandomAyah = (surahNo) => {
   return Math.ceil(Math.random() * 200) - 1;
 };
+// const getAyah = async () => {
+//   const res = await fetch(
+//     `https://tafsir.app/get.php?src=saadi&s=2&a=3&ver=1`,
+//     { mode: "no-cors" }
+//   );
+//   const data = await res.json();
+//   console.log(data);
+
+//   ayah.innerHTML = data;
+// };
+
+// const getRandomAyah = (surahNo) => {
+//   return Math.ceil(Math.random() * 200) - 1;
+// };
 getAyah();
 ayahBtn.addEventListener("click", getAyah);
